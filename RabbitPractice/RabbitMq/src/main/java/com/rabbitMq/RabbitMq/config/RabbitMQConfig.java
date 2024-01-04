7845c332fde02c453da9da5abafe5eb45d2b5af7
mahendravanshi <mahendrac121@gmail.com>
2024-01-04 23:30:08 +0530

    added publisher and docker setup

--- RabbitPractice/RabbitMq/src/main/java/com/rabbitMq/RabbitMq/config/RabbitMQConfig.java
+++ RabbitPractice/RabbitMq/src/main/java/com/rabbitMq/RabbitMq/config/RabbitMQConfig.java
@@ -86,7 +86,7 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
-    
+
 }
 
 
--- RabbitPractice/RabbitMq/src/main/java/com/rabbitMq/RabbitMq/controller/MeessageJsonController.java
+++ RabbitPractice/RabbitMq/src/main/java/com/rabbitMq/RabbitMq/controller/MeessageJsonController.java
@@ -2,11 +2,13 @@ package com.rabbitMq.RabbitMq.controller;
 
 import com.rabbitMq.RabbitMq.dto.User;
 import com.rabbitMq.RabbitMq.publisher.RabbitMQJsonProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class MeessageJsonController {
@@ -16,6 +18,7 @@ public class MeessageJsonController {
 
     @PostMapping("/jsonPublish")
     public ResponseEntity<User> sendJsonMessage(@RequestBody User user){
+        log.info("user: {}",user);
         rabbitMQJsonProducer.sendJsonMessage(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
