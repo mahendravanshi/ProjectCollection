e8a8763fd93db670315633aef7ebb9edb4876658
mahendravanshi <mahendrac121@gmail.com>
2024-01-22 23:20:37 +0530

    stream1

---
+++ Stream/Stream1/src/main/java/com/stream/Stream1Application.java
@@ -0,0 +1,13 @@
+package com.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stream1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stream1Application.class, args);
+	}
+
+}
---
+++ Stream/Stream1/src/test/java/com/stream/Stream1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
