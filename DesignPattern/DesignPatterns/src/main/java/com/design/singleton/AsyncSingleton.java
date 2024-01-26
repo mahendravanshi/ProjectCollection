c39249dc0af7edf5f6c60fea6fefa223f8cd3099
mahendravanshi <mahendrac121@gmail.com>
2024-01-26 23:35:31 +0530

    added async pattern singleton

---
+++ DesignPattern/DesignPatterns/src/main/java/com/design/singleton/AsyncSingleton.java
@@ -0,0 +1,38 @@
+package com.design.singleton;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncSingleton {
+
+    private static CompletableFuture<AsyncSingleton> instance;
+
+    private AsyncSingleton() {
+        // Private constructor to prevent instantiation.
+    }
+
+    public static CompletableFuture<AsyncSingleton> getInstance() {
+        if (instance == null) {
+            synchronized (AsyncSingleton.class) {
+                if (instance == null) {
+                    instance = CompletableFuture.supplyAsync(() -> new AsyncSingleton());
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void performAsyncOperation() {
+        System.out.println("Asynchronous operation performed by the singleton instance.");
+    }
+
+    public static void main(String[] args) {
+        CompletableFuture<AsyncSingleton> singletonFuture = AsyncSingleton.getInstance();
+
+        singletonFuture.thenAccept(instance -> {
+            instance.performAsyncOperation();
+        });
+    }
+
+    
+}
+
